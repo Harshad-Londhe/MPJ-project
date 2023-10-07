e4e78a6a70ce7bedd30e51266d3c3a7449fad7a4
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2023-10-08 03:00:38 +0530

    Update DBconnection.java

--- src/main/java/projectpackage1/DBconnection.java
+++ src/main/java/projectpackage1/DBconnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class DBconnection {
 	
-	private static String url = "jdbc:mysql://localhost:3306/omos";
+	private static String url = "jdbc:mysql://localhost:3306/javaproj2y1s";
 	private static String user = "root";
 	private static String pass = "1230";
 	
