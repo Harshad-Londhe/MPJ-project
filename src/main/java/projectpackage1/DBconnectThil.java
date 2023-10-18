65f66cfb8106f890c720636f18b3a6a8b5b68347
Thilina Lakshan <127662816+Th1l1na@users.noreply.github.com>
2023-10-18 12:06:18 +0530

    create db connect

---
+++ src/main/java/projectpackage1/DBconnectThil.java
@@ -0,0 +1,31 @@
+package projectpackage1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBconnectThil {
+	
+	//create database connection
+		private static String url = "jdbc:mysql://localhost:3306/testmed";
+		private static String user = "root";
+		private static String pass = "Admin@121";
+		private static Connection con;
+		
+		public static Connection getConnection() {
+			
+			try{
+				
+				Class.forName("com.mysql.jdbc.Driver");
+				
+				con = DriverManager.getConnection(url, user, pass);
+				
+			}catch(Exception e){
+				
+				System.out.println("Database connection is not success!!!");
+			}
+			
+			return con;
+		}
+
+
+}
