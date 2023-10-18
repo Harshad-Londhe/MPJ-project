6558eb564cc90de6608f1e8171d7dd6a15bb5e53
Thilina Lakshan <127662816+Th1l1na@users.noreply.github.com>
2023-10-18 12:07:31 +0530

    update emp db util
    remove the database creation and connect the db connecrt single file.

--- src/main/java/projectpackage1/EmpDBUtil.java
+++ src/main/java/projectpackage1/EmpDBUtil.java
@@ -1,26 +1,25 @@
 package projectpackage1;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class EmpDBUtil {
+	
+	private static Connection con = null;
+	private static Statement stmt = null;
+	private static ResultSet rs = null;
 	
 	public static boolean insertEmp(String fname, String lname, String dob, String phone, String gender, String email, String pwd, String add, String job, String join, String insure, String zone) {
 		
 		boolean isSuccess = false;
 		
 		//create database connection
-		String url = "jdbc:mysql://localhost:3306/testmed";
-		String user = "root";
-		String pass = "Admin@121";
 		
 		try {
 			
-			Class.forName("com.mysql.jdbc.Driver");
-			
-			Connection con = DriverManager.getConnection(url, user, pass);
-			Statement stmt = con.createStatement();
+			con = DBconnectThil.getConnection();
+			stmt = con.createStatement();
 			
 			String sql = "insert into emp values(0, '"+fname+"', '"+lname+"', '"+dob+"', '"+phone+"', '"+gender+"', '"+email+"', '"+pwd+"', '"+add+"', '"+job+"', '"+join+"', '"+insure+"', '"+zone+"')";
 			
