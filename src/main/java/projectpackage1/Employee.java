fc0968adba2f934836629196038e6eb7fcd7c464
Thilina Lakshan <127662816+Th1l1na@users.noreply.github.com>
2023-10-21 20:57:21 +0530

    create employee java

---
+++ src/main/java/projectpackage1/Employee.java
@@ -0,0 +1,89 @@
+package projectpackage1;
+
+public class Employee {
+	private int id;
+	private String fanme;
+	private String lname;
+	private String dob;
+	private String phone;
+	private String gender;
+	private String email;
+	private String pwd;
+	private String add;
+	private String job;
+	private String join;
+	private String insure;
+	private String zone;
+	
+	public Employee(int id, String fanme, String lname, String dob, String phone, String gender, String email,
+			String pwd, String add, String job, String join, String insure, String zone) {
+		super();
+		this.id = id;
+		this.fanme = fanme;
+		this.lname = lname;
+		this.dob = dob;
+		this.phone = phone;
+		this.gender = gender;
+		this.email = email;
+		this.pwd = pwd;
+		this.add = add;
+		this.job = job;
+		this.join = join;
+		this.insure = insure;
+		this.zone = zone;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getFanme() {
+		return fanme;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public String getAdd() {
+		return add;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public String getJoin() {
+		return join;
+	}
+
+	public String getInsure() {
+		return insure;
+	}
+
+	public String getZone() {
+		return zone;
+	}
+	
+
+}
