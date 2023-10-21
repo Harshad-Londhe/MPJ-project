f331eb5a96cdfc90bc2c8e4567bf5cf0ee5d6a4e
Thilina Lakshan <127662816+Th1l1na@users.noreply.github.com>
2023-10-21 20:56:51 +0530

    update emp dbutil

--- src/main/java/projectpackage1/EmpDBUtil.java
+++ src/main/java/projectpackage1/EmpDBUtil.java
@@ -3,6 +3,8 @@ package projectpackage1;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmpDBUtil {
 	
@@ -35,7 +37,44 @@ public class EmpDBUtil {
 		
 	} //INSERT EMp CLOSE tag
 	
-	
+	public static List<Employee> getEmpDetails(String Id){
+		
+		int convertedId = Integer.parseInt(Id);
+		
+		ArrayList<Employee> emp = new ArrayList<>();
+		
+		try {
+			con = DBconnectThil.getConnection();
+			stmt = con.createStatement();
+			
+			String sql = "select * from emp where id = '"+convertedId+"'";
+			rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				int id = rs.getInt(1);
+				String fname = rs.getString(2);
+				String lname = rs.getString(3);
+				String dob =  rs.getString(4);
+				String phone = rs.getString(5);
+				String gender = rs.getString(6);
+				String email = rs.getString(7);
+				String pwd =  rs.getString(8);
+				String add =  rs.getString(9);
+				String job =  rs.getString(10);
+				String join = rs.getString(11);
+				String insure = rs.getString(12);
+				String zone = rs.getString(13);
+				
+				Employee e = new Employee(id,fname,lname,dob,phone,gender,email,pwd,add,job,join,insure,zone);
+				emp.add(e);
+				
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return emp;
+	}
 	
 
 }
